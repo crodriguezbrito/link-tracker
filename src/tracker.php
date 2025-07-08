da663ac80fd41646c90688d812a2373f3ee55dc9
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-07-08 16:24:03 +0100

    improve build if the query already has parameters

--- src/tracker.php
+++ src/tracker.php
@@ -7,12 +7,33 @@ class Tracker
 	/**
 	 * Builds a URL with query parameters.
 	 *
-	 * @param string $baseurl The base URL to which parameters will be appended.
+	 * @param string $url The URL to which parameters will be appended.
 	 * @param array $params An associative array of query parameters.
 	 * @return string The complete URL with query parameters.
 	 */
-	public static function buildlink(string $baseurl, array $params = []): string
+	public static function buildlink(string $url, array $params = []): string
 	{
-		return $baseurl . '?' . http_build_query($params);
+		
+		if ( ! empty( $params ) ) {
+			$query     = '';
+			$url_parts = parse_url($url);
+			if (empty($url_parts['query'])) {
+				$url_parts['query'] = '';
+			} else {
+				$query = $url_parts['query'];
+			}
+			
+			$query .= '&' . http_build_query($params, '', '&');
+			$query  = trim($query, '&');
+			
+			if ( empty( $url_parts['query'] ) ) {
+				$url .= '?' . $query;
+			} else {
+				$url = str_replace( $url_parts['query'], $query, $url );
+			}
+		}
+		
+		
+		return $url;
 	}
 }
